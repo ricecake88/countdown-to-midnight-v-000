4ed0abaef269aeef5b4ac9c4198c9ac5876d7543
Grace Shih <vocalise@gmail.com>
2018-05-04 06:34:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown_with_sleep(sec)
+  sleep(sec)
 end
+
+def countdown(time)
+  i = time
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    i-=1
+    countdown_with_sleep(1)    
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+
