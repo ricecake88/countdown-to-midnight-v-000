558b7d1fc80f8f39ffd7de3a7a4881a97be3468b
Grace Shih <vocalise@gmail.com>
2018-05-04 06:32:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,10 +7,10 @@ end
 def countdown(time)
   i = time
   while i > 0
-    countdown_with_sleep(5)
     puts "#{i} SECOND(S)!"
     i-=1
     puts i
+    countdown_with_sleep(1)    
   end
   return "HAPPY NEW YEAR!"
 end
